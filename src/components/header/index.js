f9a2bc4fb95f2c7a01b8ecbf6030aa50bd0b14e6
eqbit <eqbits@gmail.com>
2019-11-19 13:45:08 +0500

    initial

---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = props => (
+  <header className="header">
+    <a href="/">test</a>
+  </header>
+);
+
+export {Header};
---
+++ src/pages/front-page/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Header} from '../../components/header';
+
+const FrontPage = props => (
+  <div className="wrapper">
+    <Header />
+    <div className="container">test</div>
+  </div>
+);
+
+export {FrontPage};
